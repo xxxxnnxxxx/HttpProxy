9c4a555e3249e86fb9d8e7fbcd51936a4c9f56ed
xxxxnnxxxx <xxxxnnxxxx@126.com>
2016-09-07 20:05:34 +0800

    可以输入参数，issuser和password

--- CertGen/certgen.cpp
+++ CertGen/certgen.cpp
@@ -1,10 +1,27 @@
 #include <Windows.h>
 #include <stdio.h>
 #include "..\HttpProxy\CertificateProvider.h"
+#include "getopt.h"
 
 #define INI_NAME        "conf.ini"
 #define RootCert_NAME   "RootCert.pem"
 #define PriKey_NAME     "PriKey.pem"
+#define RCConf_NAME     "RootCertConfig.h"  //生成配置文件
+
+
+
+//cmdline paramters
+#define PARAM_OUTPUTDIR 0x00000001
+#define PARAM_ISSUSER   0x00000002
+#define PARAM_PWD       0x00000004
+
+
+#define RCConf_Content(x,y)\
+    "#ifndef _ROOTCERTCONFIG_H_\r\n" \
+    "#define _ROOTCERTCONFIG_H_\r\n" \
+    "#define ISSUSER \""## x ##  "\"\r\n" \
+    "#define PASSWORD \"" ## y ## "\"\r\n" \
+    "#endif"
 
 typedef struct _RC_CONF_
 {
@@ -32,52 +49,130 @@ void ImportConfInfo(PRC_CONF pconf)
 
 }
 
+
+void GenRCConfHeader(const char* file,const char * content, size_t len)
+{
+    FILE *fp = NULL;
+    errno_t error = 0;
+    size_t wsize = 0;
+
+    if( file == NULL)
+        return;
+
+    error = fopen_s(&fp,file,"wb+");
+
+    if(error == 0){
+    
+        wsize = fwrite(content, 1, len,fp);
+        
+    }
+    
+    if( fp != NULL) fclose( fp );
+}
+
 int main(int argc, char **argv)
 {
     char szPriKeyPath[MAX_PATH] = {0};
     char szRootCertPath[MAX_PATH] = {0};
+    char szRCConf[MAX_PATH] = {0}; 
     RC_CONF rc_conf;
     char * szParamter = NULL;
     char szOutputDir[MAX_PATH] = {0};
+    int c;
+    int digit_optind = 0;
+    int option_index = 0;
+    char szIssuser[200] = {0};
+    char szPwd[200] = {0};
+    unsigned int opt = 0;
+    EVP_PKEY *pKey = NULL;
+    X509 *x509 = NULL;
+    char  rcconf_header[200] = {0};    
 
-    if(argc!=2)
-        return -1L;
 
-    szParamter = argv[1];
+    while(1){
+        static struct option long_options[] = {
+            {"outputdir", required_argument, 0,  0 },
+            {"issuer",  required_argument, 0,  0 },
+            {"password", required_argument, 0, 0},
+            {0,         0,                 0,  0 }
+        };
 
-    GetFullPathNameA(szParamter,MAX_PATH,szOutputDir,NULL);
 
+        c = getopt_long(argc, argv, "o:i:p:",
+            long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch(c){
+        case 'o':
+            {
+                if(optarg != NULL){
+                    szParamter = optarg;
+                    GetFullPathNameA(szParamter,MAX_PATH,szOutputDir,NULL);
+                    opt |= PARAM_OUTPUTDIR;                    
+                }
+            }
+            break;
+        case 'i':
+            {
+                if(optarg != NULL){
+                    strcpy_s(szIssuser, 200, optarg);
+                    opt |= PARAM_ISSUSER;
+                }
+            }
+            break;
+        case 'p':
+            {
+                if(optarg != NULL){
+                    strcpy_s(szPwd, 200, optarg);
+                    opt |= PARAM_PWD;
+                }
+            }
+            break;
+        }
     
-    ImportConfInfo(&rc_conf);
-    
-    EVP_PKEY *pKey = CertificateProvider::generate_keypair(2048);
-    if (pKey == NULL)
-        return -1;
+    }
+    if( opt & PARAM_OUTPUTDIR ){
 
-    X509 *x509 = CertificateProvider::generate_certificate(pKey, rc_conf.szO, rc_conf.szOU, rc_conf.szCN,rc_conf.valdays);
+        ImportConfInfo(&rc_conf);
+        pKey = CertificateProvider::generate_keypair(2048);
 
-    if (x509 == NULL)/*这个地方还是存在问题，应当在为空的情况下释放m_rootkeypair*/
-    {
-        EVP_PKEY_free(pKey);
-        pKey=NULL;
-        return -1;
+        if (pKey == NULL)
+            return -1;
+
+        x509 = CertificateProvider::generate_certificate(pKey, rc_conf.szO, rc_conf.szOU, rc_conf.szCN,rc_conf.valdays);
+
+        if (x509 == NULL)
+        {
+            EVP_PKEY_free(pKey);
+            pKey=NULL;
+            return -1;
+        }
+
+        //保存密钥和证书到文件
+        strcpy_s(szPriKeyPath,sizeof(szPriKeyPath), szOutputDir);
+        strcat_s(szPriKeyPath,sizeof(szPriKeyPath), PriKey_NAME);
+
+        strcpy_s(szRootCertPath,sizeof(szRootCertPath), szOutputDir);
+        strcat_s(szRootCertPath,sizeof(szRootCertPath), RootCert_NAME);
+
+        CertificateProvider::savePriKeytofile(pKey, szPriKeyPath);
+        CertificateProvider::saveX509tofile(x509, szRootCertPath);
+
+        OPENSSL_free(x509);
+        OPENSSL_free(pKey);
     }
 
-    //保存密钥和证书到文件
-    strcpy_s(szPriKeyPath,sizeof(szPriKeyPath), szOutputDir);
-    strcat_s(szPriKeyPath,sizeof(szPriKeyPath), PriKey_NAME);
-
-    strcpy_s(szRootCertPath,sizeof(szRootCertPath), szOutputDir);
-    strcat_s(szRootCertPath,sizeof(szRootCertPath), RootCert_NAME);
-
-    CertificateProvider::savePriKeytofile(pKey, szPriKeyPath);
-    CertificateProvider::saveX509tofile(x509, szRootCertPath);
-
-    OPENSSL_free(x509);
-    OPENSSL_free(pKey);
-
-
-
+    if( opt & PARAM_ISSUSER  && opt & PARAM_PWD ){
+        strcpy_s(szRCConf,sizeof(szRCConf), szOutputDir);
+        strcat_s(szRCConf,sizeof(szRCConf), RCConf_NAME);
+        
+        wsprintfA(rcconf_header, RCConf_Content("%s","%s"), szIssuser, szPwd);
+    
+        GenRCConfHeader(szRCConf, rcconf_header, strlen(rcconf_header));
+        
+        
+    }
 
     return 0L;
 }
